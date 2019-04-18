042b8813a01af32f860f45fca14803b343997ad3
YuanZH <389937586@qq.com>
2019-04-18 15:27:46 +0800

    abc

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es6': true
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018
+    },
+    'rules': {
+        "no-console": "off",
+        'indent': [
+            'error',
+            2
+        ],
+        'linebreak-style': [
+            'warn',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};
